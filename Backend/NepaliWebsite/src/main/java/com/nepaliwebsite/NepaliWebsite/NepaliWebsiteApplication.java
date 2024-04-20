b3a4aa4e632a8b79b41a05d8a02cecb1bb329f5a
Daniel <Daniel@enculturatewichita.com>
2024-04-20 11:28:53 -0500

    added backend spring boot project template

---
+++ Backend/NepaliWebsite/src/main/java/com/nepaliwebsite/NepaliWebsite/NepaliWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.nepaliwebsite.NepaliWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NepaliWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NepaliWebsiteApplication.class, args);
+	}
+
+}
---
+++ Backend/NepaliWebsite/src/test/java/com/nepaliwebsite/NepaliWebsite/NepaliWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nepaliwebsite.NepaliWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NepaliWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
